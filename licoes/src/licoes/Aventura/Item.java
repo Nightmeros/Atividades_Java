6c382d9313f2da679df3286f7cbbf0b2501f47e8
GuilhermeP <93324394+Nightmeros@users.noreply.github.com>
2025-11-23 18:52:46 -0300

    Delete licoes/src/licoes/Aventura directory

--- licoes/src/licoes/Aventura/Item.java
+++
@@ -1,5 +0,0 @@
-public class Item {
-    public int id;
-    public String nome;
-    public String descricao;
-}
--- licoes/src/licoes/Aventura/Sala.java
+++
@@ -1,4 +0,0 @@
-public class Sala {
-    public int id;
-    public String descricao;
-}
